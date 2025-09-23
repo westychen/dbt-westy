7ae556810df990c148bf80af05465ad5a2a6b163
westy.chen <130695124+westychen@users.noreply.github.com>
2025-09-23 11:43:59 -0400

    Update assert_facts_nativex_flyers_freshness.sql

--- tests/assert_facts_nativex_flyers_freshness.sql
+++ tests/assert_facts_nativex_flyers_freshness.sql
@@ -8,4 +8,4 @@ from (
     select max(date) as max_date
     from {{ source('databricks_uc_sources', 'facts_nativex_flyers') }}
 )
-where max_date != date(current_timestamp())
+where max_date != date_sub(date(current_timestamp()), 1)
